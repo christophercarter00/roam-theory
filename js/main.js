c5b8772f70014e64daba7c3f7b6effa2f9e8ee42
Christopher Carter <150073299+christophercarter00@users.noreply.github.com>
2025-02-26 03:55:51 -0500

    Enhance navbar functionality with scrollspy and improve AOS initialization settings

--- js/main.js
+++ js/main.js
@@ -1,57 +1,66 @@
 (function ($) {
-  "use strict";
+    "use strict";
 
-  // AOS ANIMATIONS
-  AOS.init();
+    // Initialize AOS (Animate on Scroll)
+    AOS.init({
+        duration: 1000,
+        easing: 'ease-in-out',
+        once: true
+    });
 
-  // NAVBAR TOGGLE ON LINK CLICK
-  $('.navbar-nav .nav-link').click(function () {
-      $('.navbar-collapse').collapse('hide');
-  });
+    // Navbar Toggle on Link Click
+    $('.navbar-nav .nav-link').click(function () {
+        $('.navbar-collapse').collapse('hide');
+    });
 
-  // SMOOTH SCROLLING TO SECTIONS
-  $('a[href*="#"]').click(function (event) {
-      if (
-          location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') &&
-          location.hostname == this.hostname
-      ) {
-          var target = $(this.hash);
-          target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-          if (target.length) {
-              event.preventDefault();
-              $('html, body').animate(
-                  {
-                      scrollTop: target.offset().top - 66,
-                  },
-                  1000
-              );
-          }
-      }
-  });
+    // Smooth Scrolling
+    $('a[href*="#"]').on('click', function (event) {
+        if (
+            location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') &&
+            location.hostname == this.hostname
+        ) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                event.preventDefault();
+                $('html, body').animate(
+                    {
+                        scrollTop: target.offset().top - 66,
+                    },
+                    800
+                );
+            }
+        }
+    });
 
-  // STICKY NAVBAR
-  $(document).ready(function () {
-      $('.navbar').sticky({ topSpacing: 0 });
-  });
+    // Sticky Navbar
+    $(document).ready(function () {
+        $('.navbar').sticky({ topSpacing: 0 });
+    });
 
-  // MAGNIFIC POPUP FOR IMAGES
-  $(document).ready(function () {
-      $('.image-popup').magnificPopup({
-          type: 'image',
-          removalDelay: 300,
-          mainClass: 'mfp-with-zoom',
-          gallery: {
-              enabled: true,
-          },
-          zoom: {
-              enabled: true,
-              duration: 300,
-              easing: 'ease-in-out',
-              opener: function (openerElement) {
-                  return openerElement.is('img') ? openerElement : openerElement.find('img');
-              },
-          },
-      });
-  });
+    // Magnific Popup for Images
+    $(document).ready(function () {
+        $('.image-popup').magnificPopup({
+            type: 'image',
+            removalDelay: 300,
+            mainClass: 'mfp-with-zoom',
+            gallery: {
+                enabled: true
+            },
+            zoom: {
+                enabled: true,
+                duration: 300,
+                easing: 'ease-in-out',
+                opener: function (openerElement) {
+                    return openerElement.is('img') ? openerElement : openerElement.find('img');
+                }
+            }
+        });
+    });
 
-})(window.jQuery);
+    // Scrollspy Activation
+    $(document).ready(function () {
+        $('body').scrollspy({ target: '#navbarNav', offset: 70 });
+    });
+
+})(jQuery);
