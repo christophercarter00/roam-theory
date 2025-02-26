5517febebea4bc04497690bae665448c4f18b97a
Christopher Carter <150073299+christophercarter00@users.noreply.github.com>
2025-02-26 03:41:04 -0500

    Add initial project files including HTML structure, CSS styles, and JavaScript functionality

---
+++ js/main.js
@@ -0,0 +1,57 @@
+(function ($) {
+  "use strict";
+
+  // AOS ANIMATIONS
+  AOS.init();
+
+  // NAVBAR TOGGLE ON LINK CLICK
+  $('.navbar-nav .nav-link').click(function () {
+      $('.navbar-collapse').collapse('hide');
+  });
+
+  // SMOOTH SCROLLING TO SECTIONS
+  $('a[href*="#"]').click(function (event) {
+      if (
+          location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') &&
+          location.hostname == this.hostname
+      ) {
+          var target = $(this.hash);
+          target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+          if (target.length) {
+              event.preventDefault();
+              $('html, body').animate(
+                  {
+                      scrollTop: target.offset().top - 66,
+                  },
+                  1000
+              );
+          }
+      }
+  });
+
+  // STICKY NAVBAR
+  $(document).ready(function () {
+      $('.navbar').sticky({ topSpacing: 0 });
+  });
+
+  // MAGNIFIC POPUP FOR IMAGES
+  $(document).ready(function () {
+      $('.image-popup').magnificPopup({
+          type: 'image',
+          removalDelay: 300,
+          mainClass: 'mfp-with-zoom',
+          gallery: {
+              enabled: true,
+          },
+          zoom: {
+              enabled: true,
+              duration: 300,
+              easing: 'ease-in-out',
+              opener: function (openerElement) {
+                  return openerElement.is('img') ? openerElement : openerElement.find('img');
+              },
+          },
+      });
+  });
+
+})(window.jQuery);
